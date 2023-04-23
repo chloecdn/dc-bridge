00badcba96d15be3e55c8d56559077252c840536
ChloeCDN <chloe@chloedev.de>
2023-04-23 20:35:47 +0200

    Only accept messages from general-channel, fix my stupidity again.

--- src/main/java/de/chloedev/dcbridge/event/DiscordChatListener.java
+++ src/main/java/de/chloedev/dcbridge/event/DiscordChatListener.java
@@ -1,5 +1,6 @@
 package de.chloedev.dcbridge.event;
 
+import de.chloedev.dcbridge.discord.TextChannel;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
@@ -9,7 +10,10 @@ public class DiscordChatListener extends ListenerAdapter {
     public void onMessageReceived(MessageReceivedEvent e) {
         // replace the prefix with whatever we use, if we're going to use one at all, otherwise just remove it.
         if (e.getAuthor().isBot() || e.getMessage().getContentRaw().startsWith(";")) {
-            String message = e.getMessage().getContentRaw(); // i prefer raw, do what you gotta do.
+            return;
+        }
+        if (e.getChannel().getId().equals(TextChannel.GENERAl.getId())) { // only accept messages from general
+            String message = e.getMessage().getContentRaw(); // i prefer raw, do what you gotta do.}
             // sendMessageToServer(message);
         }
     }
